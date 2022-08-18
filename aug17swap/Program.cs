678329b1b1768e99cc2c279b37b4f9a985aef2c4
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-18 16:49:52 +0530

    changed

---
+++ aug17swap/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace aug17swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            Console.WriteLine("Enter i and j");
+            i = Convert.ToInt32(Console.ReadLine());
+            j= Convert.ToInt32(Console.ReadLine());
+            swap(i, j);
+            Console.WriteLine("Actual numbers: {0} and {1}", i, j);
+            gswap(ref i, ref j);
+            Console.WriteLine("Actual numbers: {0} and {1}", i, j);
+            Console.ReadLine();
+        }
+        static void swap(int i,int j)
+        {
+            int k;
+            k = i;
+            i = j;
+            j = k;
+            Console.WriteLine("Swapped numbers: {0} and {1}", i, j);
+        }
+        static void gswap(ref int i,ref int j)
+        {
+            int k;
+            k = i;
+            i = j;
+            j = k;
+            Console.WriteLine("Swapped numbers: {0} and {1}", i, j);
+
+
+        }
+    }
+}
